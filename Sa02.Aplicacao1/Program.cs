f70105eb7251c555503784761ecd65c5de1dffcb
welhingt0nBarbosa <wellingtonerudito@gmail.com>
2023-02-03 15:49:35 -0300

    Aplicativo N2

---
+++ Sa02.Aplicacao1/Program.cs
@@ -0,0 +1,27 @@
+﻿using Sa02.RegrasDeNegocio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sa02.Aplicacao1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Aplicativo2RN aplicativo2RN = new Aplicativo2RN();
+
+            Console.WriteLine("Digite o ano desejado*1968*: ");
+
+            int data = int.Parse(Console.ReadLine());
+            int diferencaData = aplicativo2RN.OperacaoData(data);
+
+            Console.WriteLine($"diferença: {diferencaData}");
+
+
+            
+        }
+    }
+}
---
+++ Sa02.RegrasDeNegocio/Aplicativo2RN.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sa02.RegrasDeNegocio
+{
+    public class Aplicativo2RN
+    {
+        public int OperacaoData(int data)
+        {
+            int anoAtual = DateTime.Today.Year;
+
+            int diferenca = anoAtual- data;
+
+            return diferenca;
+
+
+        }
+
+
+    }
+}
